4d63d964dad17e5c00029c8b8cb848475d076540
tylerjnorton <tyler.j.norton@gmal.com>
2015-11-13 17:07:13 -0500

    if this is wrong just roll back the commit

--- dist/js/scoreboard.js
+++ dist/js/scoreboard.js
@@ -1,50 +1,37 @@
 (function () {
+	'use strict';
 
-	var app = angular.module('scoreboard', []);	
+	var angular = require('angular');
 
-	app.factory('socket', function ($rootScope) {
+	require('angular-socket-io');
 
-		var socket = io.connect();
-		console.log('New Socket Connection');
-
-		return {
-			on: function (eventName, callback) {
-				socket.on(eventName, function () {  
-					var args = arguments;
-					$rootScope.$apply(function () {
-						callback.apply(socket, args);
-					});
-				});
-			},
-			emit: function (eventName, data, callback) {
-				socket.emit(eventName, data, function () {
-					var args = arguments;
-					$rootScope.$apply(function () {
-						if (callback) {
-							callback.apply(socket, args);
-						}
-					});
-				})
-			}
-		};
-	});
-
-	app.controller('ScoreboardController', function ($scope, socket) {
+	angular.module('scoreboard', [
+		'btford.socket-io'
+	])
+	.factory('mySocket', ( socketFactory ) => {
+		return socketFactory();
+	})
+	.controller('ScoreboardCtrl', ( $scope, mySocket ) => {
 		$scope.teams = [];
-		$scope.title = 'Trivia';
+		$scope.game = { title: 'Trivia' };
 		$scope.currentBuzzer = null;
 
-		socket.on('teams', function (data) {
+		mySocket.on('game', function ( data ) {
+			console.log('Game:', data);
+			$scope.game = data;
+		});
+
+		mySocket.on('teams', function (data) {
 			console.log('Teams:', data);
 			$scope.teams = data;
 		});
 
-		socket.on('round', function (data) {
+		mySocket.on('round', function (data) {
 			console.log('Round:', data);
 			$scope.round = data;
 		});
 
-		socket.on('buzz', function (data) {
+		mySocket.on('buzz', function (data) {
 			console.log('Buzz', data);
 			$scope.currentBuzzer = data.name;
 		});
